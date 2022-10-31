cdc0bd15353ae6faad51732f23cd3d91de879008
Maxwell Dodd <doddm2@wwu.edu>
2022-10-31 11:15:39 -0700

    Adding Files

---
+++ FoodApp/src/Controller.java
@@ -0,0 +1,52 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private Button CreateTemplateButton;
+
+    @FXML
+    private Button FoodTypesLessonButton;
+
+    @FXML
+    private Button GroceryLessonButton;
+
+    @FXML
+    private Button GroceryLessonButton111;
+
+    @FXML
+    private Button MealPopUpBackButton;
+
+    @FXML
+    private ImageView RequiredLessonOne;
+
+    @FXML
+    private Tab TabInspiration;
+
+    @FXML
+    private Tab TabLesson;
+
+    @FXML
+    private TabPane Tabs;
+
+    @FXML
+    private Button TagOneButton;
+
+    @FXML
+    private Button TemplateButtonTwo;
+
+    @FXML
+    private Label TemplateLabelOne;
+
+    @FXML
+    private Label TemplateLabelTwo;
+
+    @FXML
+    private Button ToolsLessonButton;
+
+}
